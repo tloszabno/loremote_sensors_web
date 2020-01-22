72c9f2a84f269664d9490593f2e89cd5c4f67543
Tomasz Los <tloszabno@gmail.com>
2020-01-22 17:17:09 +0100

    add time to view

--- src/components/SensorsBoard.tsx
+++ src/components/SensorsBoard.tsx
@@ -2,17 +2,24 @@ import React, { useEffect, useState } from "react";
 import { MeasurementsSet, Measurement } from "./SensorTypes";
 import { SensorBadge } from "./SensorBadge";
 import { SensorsBoardDiv } from "./SensorsBoard.styled";
+import { Badge } from "reactstrap";
 
-const INTERVAL_IN_MS = 1* 60 * 1000;
+const INTERVAL_IN_MS = 1 * 60 * 1000;
 
 export const SensorsBoard: React.FC = () => {
   const [data, setData] = useState<{ [id: string]: Measurement[] }>({});
+  const [measurementTime, setMeasurementTime] = useState<Date | null>(null);
   const [reload, setReload] = useState<boolean>(true);
   useEffect(() => {
     if (reload) {
       fetch("/api/measurements/1")
         .then(res => res.json())
-        .then(data => groupBySensorName(data.data[0]))
+        .then(data => {
+          const measurementSet = data.data[0];
+          console.log(measurementSet);
+          setMeasurementTime(measurementSet.timestamp);
+          return groupBySensorName(measurementSet);
+        })
         .then(data => {
           setData(data);
           setReload(false);
@@ -27,15 +34,20 @@ export const SensorsBoard: React.FC = () => {
   }, []);
 
   return (
-    <SensorsBoardDiv>
-      {Object.keys(data).map(key => (
-        <SensorBadge
-          key={`${key}-sensor-panel`}
-          name={key}
-          values={data[key]}
-        />
-      ))}
-    </SensorsBoardDiv>
+    <>
+      <SensorsBoardDiv>
+        {Object.keys(data).map(key => (
+          <SensorBadge
+            key={`${key}-sensor-panel`}
+            name={key}
+            values={data[key]}
+          />
+        ))}
+      </SensorsBoardDiv>
+      <div className="text-muted">
+          Measurements time: {measurementTime}
+      </div>
+    </>
   );
 };
 
